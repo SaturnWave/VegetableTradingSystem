c37d88d37c75e83ba74b6f2e9c50d342b8ad1f47
Arcan <achome7203@gmail.com>
2023-10-02 20:26:01 -0400

    Initial commit

---
+++ Remanings.py
@@ -0,0 +1,133 @@
+# <!--  Main Navigation-->
+#     <!-- <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+#   <div class="container-fluid">
+    
+   
+#     <a class="navbar-brand" href="#">COMP229</a>-->
+#     <!--<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+#       <span class="navbar-toggler-icon"></span>
+#     </button>-
+#     <img src="/img/logo.png" alt="logo" style: width="100px"; height="100px">
+#     <div class="collapse navbar-collapse" id="navbarSupportedContent">
+       
+#       <b>
+#         <ul class="navbar-nav ml-auto">
+
+#           <% if (title=="Home") { %>
+#         <li class="nav-item active">
+#           <% } else { %>
+#             <li class="nav-item">
+#               <% } %>
+#           <a class="nav-link active"  aria-content=page href="/"><i class="fas fa-lg fa-laptop-house"></i> Home </a>
+#         </li>
+
+#          <% if (title=="About") { %>
+#         <li class="nav-item active">
+#           <% } else { %>
+#             <li class="nav-item">
+#               <% } %>
+#           <a class="nav-link"  href="/about"><i class="fa-solid fa-address-card"></i> About </a>
+#         </li>
+
+#         <% if (title=="Contact") { %>
+#           <li class="nav-item active">
+#           <% } else { %>
+#         <li class="nav-item">
+#           <% } %>
+#           <a class="nav-link"  href="/contact"><i class="fa-solid fa-address-book"></i>Contact</a>
+#         </li>
+
+#          <% if (title=="Projects") { %>
+#           <li class="nav-item active">
+#           <% } else { %>
+#         <li class="nav-item">
+#           <% } %>
+#           <a class="nav-link"  href="/projects"><i class="fa-brands fa-product-hunt"></i>Projects</a>
+#         </li>
+
+
+#          <%if(title=="Services"){%>
+#           <li class="nav-item active">
+#           <% } else { %>
+#         <li class="nav-item">
+#           <% } %>
+#           <a class="nav-link"  href="/services"><i class="fa-brands fa-servicestack"></i>Services</a>
+#         </li>
+#         </ul>
+#         </b>
+#     </div>
+#   </div> 
+# </nav>
+
+#  body
+#             {
+#                 font-family: Comic sans MS;
+#             }
+#         ul.navbar-nav {
+#             list-style: none; /* Remove the bullet */
+#             display: inline; /* Display the list items inline */
+#             float:right;
+#             font-size:20px
+            
+#         }
+        
+#         ul.navbar-nav li {
+#             display: inline-block; /* Make the list items display inline */
+#         }
+#         a:link {
+#     text-decoration: none;
+# }
+
+
+
+
+# a:visited {
+#     text-decoration: none;
+# }
+
+
+# a:hover {
+#     text-decoration: none;
+# }
+
+
+# a:active {
+#     text-decoration: none;
+# }
+
+# html,
+# body {
+#   height: 100%;
+#   margin: 0;
+# }
+
+# body {
+#   display: flex;
+#   flex-direction: column;
+# }
+
+# main {
+#     align-items: center;
+#   flex: 1;
+# }
+
+
+#  footer {
+#             background-color: #297dd2;
+#             color: #ffffff;
+#             padding: 5px;
+#             opacity: 0.8;
+#             text-align: center;
+#             font-size: 20px;
+#             font-family: 'Comic Sans MS', cursive, sans-serif;
+
+            
+#         }
+
+#         footer .navbar-text {
+#             margin-bottom: 0;
+#             text-align: center;
+#         }
+
+
+#     </style>
