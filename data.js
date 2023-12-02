3de2f11b08d37e1a4f314cf3f93f56e104709d67
Arcan <achome7203@gmail.com>
2023-12-01 21:35:17 -0500

    setting up login page

---
+++ data.js
@@ -0,0 +1,105 @@
+const vegetables = [
+    { 
+        name: 'broccoli', 
+        color: 'green', 
+        price: 1.59, 
+        quantity: 1, 
+        id: 1, 
+        weight: 1, 
+        category: 'vegetable', 
+        image: 'https://images.unsplash.com/photo-1581093455538-1a5b8b5e9c9f?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YnJvY2NvbGklMjB3ZWF0aGVyJTIwY2FyfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80'
+    },
+    {
+        name: 'carrot',
+        color: 'orange',
+        price: 0.99,
+        quantity: 1,
+        id: 2,
+        weight: 1,
+        category: 'vegetable',
+        image: 'https://images.unsplash.com/photo-1581093455538-1a5b8b5e9c9f?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YnJvY2NvbGklMjB3ZWF0aGVyJTIwY2FyfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80'
+    },
+    {
+        name: 'tomato',
+        color: 'red',
+        price: 1.99,
+        quantity: 1,
+        id: 3,
+        weight: 1,
+        category: 'vegetable',
+        image: 'https://images.unsplash.com/photo-1581093455538-1a5b8b5e9c9f?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YnJvY2NvbGklMjB3ZWF0aGVyJTIwY2FyfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80'
+    },
+    {
+        name: 'potato',
+        color: 'brown',
+        price: 0.99,
+        quantity: 1,
+        id: 4,
+        weight: 1,
+        category: 'vegetable',
+        image: 'https://images.unsplash.com/photo-1581093455538-1a5b8b5e9c9f?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YnJvY2NvbGklMjB3ZWF0aGVyJTIwY2FyfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80'
+    },
+    {
+        name: 'onion',
+        color: 'white',
+        price: 1.99,
+        quantity: 1,
+        id: 5,
+        weight: 1,
+        category: 'vegetable',
+        image:'https://images.unsplash.com/photo-1581093455538-1a5b8b5e9c9f?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YnJvY2NvbGklMjB3ZWF0aGVyJTIwY2FyfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80'
+    }
+];
+
+const fruits = [
+    { 
+        name: 'apple', 
+        color: 'red', 
+        price: 1.59, 
+        quantity: 1, 
+        id: 7, 
+        weight: 1, 
+        category: 'fruit', 
+        image: 'https://images.unsplash.com/photo-1581093455538-1a5b8b5e9c9f?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YnJvY2NvbGklMjB3ZWF0aGVyJTIwY2FyfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80'
+    },
+    {
+        name: 'banana',
+        color: 'yellow',
+        price: 0.99,
+        quantity: 1,
+        id: 8,
+        weight: 1,
+        category: 'fruit',
+        image: 'https://images.unsplash.com/photo-1581093455538-1a5b8b5e9c9f?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YnJvY2NvbGklMjB3ZWF0aGVyJTIwY2FyfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80'
+    },
+    {
+        name: 'orange',
+        color: 'orange',
+        price: 1.99,
+        quantity: 1,
+        id: 9,
+        weight: 1,
+        category: 'fruit',
+        image: 'https://images.unsplash.com/photo-1581093455538-1a5b8b5e9c9f?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YnJvY2NvbGklMjB3ZWF0aGVyJTIwY2FyfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80'
+    },
+    {
+        name: 'grapes',
+        color: 'purple',
+        price: 0.99,
+        quantity: 1,
+        id: 10,
+        weight: 1,
+        category: 'fruit',
+        image: 'https://images.unsplash.com/photo-1581093455538-1a5b8b5e9c9f?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YnJvY2NvbGklMjB3ZWF0aGVyJTIwY2FyfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80'
+    }
+];
+
+const farmLands = [
+    {
+        landId: '1', 
+        area: '1500sqft', 
+        address: '9013 Pigram Road, Malahide, Ontario N0L1C0', 
+        inUse: true, 
+        landImage: 'https://images.unsplash.com/photo-1581093455538-1a5b8b5e9c9f?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bGFuZCUyMGltYWdlJTIwZm9yJTIwZmFybXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80'
+    }
+];
